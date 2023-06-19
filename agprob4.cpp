7f548eb5fcd736c39e8c4e5ffe0e0370dbd21f9d
umareddy12 <121800119+umareddy12@users.noreply.github.com>
2023-06-19 22:30:47 +0530

    Add files via upload

---
+++ agprob4.cpp
@@ -0,0 +1,19 @@
+//Reddy Uma
+//Reddy Uma
+// question-4:To find area of the rectangle
+#include<iostream>//header file
+using namespace std;
+int main()//main function
+{
+int length,breadth,area;//declaration
+//asking input
+cout<<"enter length value:"<<endl;
+cin>>length;
+cout<<"enter breadth value:"<<endl;
+cin>>breadth;
+area=length*breadth;
+//displaying output
+cout<<"Area of the rectangle:"<<area;
+return 0;	
+}
+
