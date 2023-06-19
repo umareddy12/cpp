23d0dc332ec5fb3571e1211317f28ca0cbcb7a63
umareddy12 <121800119+umareddy12@users.noreply.github.com>
2023-06-19 22:30:11 +0530

    Add files via upload

---
+++ agprob3.cpp
@@ -0,0 +1,26 @@
+//Reddy Uma
+//question-3
+/*Write a program to find value of x power 2 &x power 4 by using 
+normal method and math function method*/
+#include<iostream>//header file
+using namespace std;
+int main()//main function
+{
+	int x,n=2,pow;//declaration
+	//asking input
+	cout<<"enter x value:"<<endl;
+	cin>>x;
+	
+	//pow=power(x,n);//using power function 
+	
+	cout<<"x power 2 value is:"<<pow;
+	return 0;
+}
+	
+	//same for x power 4
+	/*int x,n=2;
+	cout<<"enter x value:"<<endl;
+	cin>>pow;
+	pow=x*n;
+	cout<<pow;
+	return 0;*/
