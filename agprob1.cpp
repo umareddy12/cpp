290247ca53ebb2ad76bc4539c39142c4c4052147
umareddy12 <121800119+umareddy12@users.noreply.github.com>
2023-06-19 22:29:07 +0530

    Add files via upload

---
+++ agprob1.cpp
@@ -0,0 +1,16 @@
+//assignment-1 question-1
+//Reddy Uma Devi
+//write a Program to print given text
+#include<iostream>//header file
+using namespace std;
+int main()//main function
+
+{
+	//printing output
+	cout<<"              ***WELCOME TO AG CODER'S BATCH-2***          "<<endl;
+	cout<<"THIS IS MY 1ST ASSIGNMENT."<<endl;
+	cout<<"NOW I'M WORKING ON \'PRINT STATEMENT'S\' AND \'endl\'"<<endl;
+	cout<<"YES, I COMPLETED MY 1ST PROBLEM.";
+	return 0;
+}
+
